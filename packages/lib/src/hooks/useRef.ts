7bb6689358a1d154267db0a371b2a7625e6f099e
이태영 <lhlhgodz3@gmail.com>
2025-07-21 23:45:14 +0900

    fix: useRef 함수 구현

--- packages/lib/src/hooks/useRef.ts
+++ packages/lib/src/hooks/useRef.ts
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
+// useRef 훅은 렌더링 사이에 값을 유지하는 가변 ref 객체를 생성합니다.
 export function useRef<T>(initialValue: T): { current: T } {
+  // 이 부분을 적절히 수정하세요. useRef를 구현하지 않으면 다른 hook을 구현할 수 없습니다.
   // useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  const [state] = useState({ current: initialValue });
+
+  return state;
 }
