e6e8475fdbe8074fe23e19cc65e510ad1605ec1d
이태영 <lhlhgodz3@innogird.com>
2025-07-24 15:53:52 +0900

    fix: ToastProvider 함수 구현 중

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -1,52 +1,71 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, memo, type PropsWithChildren, useContext, useReducer } from "react";
+import { createContext, memo, type PropsWithChildren, useContext, useMemo, useReducer } from "react";
 import { createPortal } from "react-dom";
 import { Toast } from "./Toast";
 import { createActions, initialState, toastReducer, type ToastType } from "./toastReducer";
 import { debounce } from "../../utils";
+import { useCallback } from "@hanghae-plus/lib/src/hooks";
 
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastContext = createContext<{
+// 불필요한 리렌더링을 방지하기 위해 상태와 명령을 각각 분리함
+const ToastStateContext = createContext<{
   message: string;
   type: ToastType;
+}>(initialState);
+
+const ToastCommandContext = createContext<{
   show: ShowToast;
   hide: Hide;
 }>({
-  ...initialState,
   show: () => null,
   hide: () => null,
 });
 
 const DEFAULT_DELAY = 3000;
 
-const useToastContext = () => useContext(ToastContext);
-export const useToastCommand = () => {
-  const { show, hide } = useToastContext();
-  return { show, hide };
-};
-export const useToastState = () => {
-  const { message, type } = useToastContext();
-  return { message, type };
-};
+export const useToastState = () => useContext(ToastStateContext);
+export const useToastCommand = () => useContext(ToastCommandContext);
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
   const { show, hide } = createActions(dispatch);
   const visible = state.message !== "";
 
-  const hideAfter = debounce(hide, DEFAULT_DELAY);
+  const hideAfter = useMemo(() => debounce(hide, DEFAULT_DELAY), [hide]);
 
-  const showWithHide: ShowToast = (...args) => {
-    show(...args);
-    hideAfter();
-  };
+  const showWithHide: ShowToast = useCallback(
+    (...args) => {
+      show(...args);
+      hideAfter();
+    },
+    [show, hideAfter],
+  );
+
+  // 상태와 명령을 각각 분리하여 context value 생성
+  const stateValue = useMemo(
+    () => ({
+      message: state.message,
+      type: state.type,
+    }),
+    [state.message, state.type],
+  );
+
+  const commandValue = useMemo(
+    () => ({
+      show: showWithHide,
+      hide,
+    }),
+    [showWithHide, hide],
+  );
 
   return (
-    <ToastContext value={{ show: showWithHide, hide, ...state }}>
+    <>
       {children}
-      {visible && createPortal(<Toast />, document.body)}
-    </ToastContext>
+      <ToastCommandContext value={commandValue}>
+        <ToastStateContext value={stateValue}>{visible && createPortal(<Toast />, document.body)}</ToastStateContext>
+      </ToastCommandContext>
+    </>
   );
 });
